e4357a207fb6166839e30509d36993b9f65c7bd4
Andreas Holstenson <a@holstenson.se>
2025-06-07 15:42:54 +0200

    fix: Use correct path to x86_64 libraries

--- libs.go
+++ libs.go
@@ -1,7 +1,7 @@
 package loro
 
-// #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/libs/amd64-apple-darwin -lloro
+// #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/libs/x86_64-apple-darwin -lloro
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/libs/aarch64-apple-darwin -lloro
-// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libs/amd64-unknown-linux-musl -lloro
+// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libs/x86_64-unknown-linux-musl -lloro
 // #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/libs/aarch64-unknown-linux-musl -lloro
 import "C"
